6316ab492f2e0168d6fa6dd73cd000fd25687224
Bob Witmer <bwitmer@golden.net>
2023-07-15 12:36:31 -0400

    Chapter 3.2 Completed - Animation and Sound added

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,54 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.9)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false    // Shrink the image by 90% via the scaleEffect()
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {   // With Spring Animation
+                        animateImage = true // Change the image back to 100% via the scaleEffect()
+                    }
+                }
+
+
+            Spacer()
+            
+            Button {
+                //TODO: Button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
+    }
+    // Function to Play a sound
+    // Requires:    import AVFAudio
+    //              @State private var audioPlayer: AVAudioPlayer!
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName).")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer.")
+        }
     }
 }
 
