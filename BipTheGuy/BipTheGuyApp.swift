7b22022f5ae7ec33f5fc331144ac122a4ce5be10
Bob Witmer <bwitmer@golden.net>
2023-07-15 11:43:56 -0400

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Bob Witmer on 2023-07-15.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
